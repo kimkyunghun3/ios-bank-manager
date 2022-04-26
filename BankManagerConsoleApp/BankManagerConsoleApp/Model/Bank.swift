9316a5117dd493b9e224a525a0a5809f771d5c01
dudu <whqtkf12@naver.com>
2022-04-26 16:18:59 +0900

    feat: Bank의 sendCustomerToClerk 매서드 구현

--- BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/Model/Bank.swift
@@ -9,10 +9,17 @@ struct Bank {
     private let waitingQueue = Queue<Customer>()
     private let clerk = BankClerk()
 
-    func makeCustomers() {
+    private func makeCustomers() {
         let customerCount = Int.random(in: 10...30)
         for number in 0..<customerCount {
             waitingQueue.enqueue(Customer(waitingNumber: number))
+        }
+    }
+
+    private func sendCustomerToClerk() {
+        while !waitingQueue.isEmpty {
+            guard let customer = waitingQueue.dequeue() else { return }
+            clerk.work(for: customer)
         }
     }
 }
